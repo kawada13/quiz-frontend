302210701fe940aadce4c40b7433820c03dd0272
kawada <vgnsl94@gmail.com>
2021-01-03 19:30:10 +0900

    モーダル作成

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -53,21 +53,87 @@
         </v-col>
       </v-row>
     </div>
-  <div class="answer-btn">
-      <v-btn
-        color="primary"
-        x-large
-        block
-        depressed
-      >
-        回答する
-      </v-btn>
-  </div>
+
+<div class="dialog">
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+          x-large
+          block
+          depressed
+        >
+          回答する
+        </v-btn>
+      </template>
+      <div class="white py-4">
+        <div>
+          <div class="text-center blue--text judge-text" v-if="true">
+            ○正解！
+          </div>
+          <div class="text-center red--text judge-text" v-else >
+            ×不正解！
+          </div>
+          <div class="text-center font-weight-bold answear-text">
+            正解は「A: 富士山」
+          </div>
+          <div class="py-3 px-6">
+            説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明
+          </div>
+          <div class="mt-3">
+            <v-btn
+              color="cyan darken-1 white--text"
+              dark
+              v-on="on"
+              depressed
+              @click="dialog = false"
+              class="text-center qqq mb-4"
+            >
+              次の問題
+            </v-btn>
+            <v-btn
+              color="primary"
+              text
+              v-on="on"
+              @click="dialog = false"
+              class="text-center qqq"
+            >
+              やめる
+            </v-btn>
+          </div>
+        </div>
       </div>
+    </v-dialog>
+  </v-row>
+</div>
+  </div>
   </v-container>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
 
 <style>
+.judge-text{
+  font-size: 40px;
+}
+.answear-text{
+  font-size: 22px;
+}
 .question-container{
   margin: 0 30px;
 }
@@ -83,5 +149,9 @@
 }
 .answer-btn{
   margin-top: 50px;
+}
+.qqq {
+  display: block;
+  margin: 0 auto;
 }
 </style>
